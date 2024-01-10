86f4484fdefc6c8a1e1ea5e5e5bf8feb85c56a51
GuilhermeBrag <guibragadantas@hotmail.com>
2024-01-10 19:43:18 -0300

    first commit

---
+++ src/test/java/com/spring/restspringbootjava/RestSpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restspringbootjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
